cd5bc82759996e5e5e5e62a5a4ff570d335e2e97
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-10 12:10:21 -0400

    flash-common: bluetooth: Disable clean turn on
    * This isn't needed anymore.
    
    Change-Id: Ib77e5212099d82bbc07aea6044eb35109faff5cb

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -31,8 +31,6 @@
 
 #define BLE_VND_INCLUDED   TRUE
 #define GATT_MAX_PHY_CHANNEL  10
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 35
