d4c6047aa09cb8d60842703ae12a9b0f9e99d3fc
SGCMarkus <markusornik@gmail.com>
2020-08-22 22:31:33 +0200

    flash-common: bluetooth: raise MAX_L2CAP_CHANNELS to 32

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,7 +27,7 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 
 #define BLE_VND_INCLUDED   TRUE
 #define GATT_MAX_PHY_CHANNEL  10
